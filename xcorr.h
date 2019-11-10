50bb8deed949e1b9052f470e937ab1089590322b
Jon Blank <jlblank@wisc.edu>
2019-11-09 22:37:44 -0500

    initial creation of xcorr library and function

---
+++ xcorr.h
@@ -0,0 +1,9 @@
+// Author: Jon Blank
+
+#ifndef XCORR
+#define XCORR
+
+// reads in two arrays of known lengths and computed the cross correlation
+double * xcorr(double *signalOne, double *signalTwo, double *signalOneTime, double *signalTwoTime, double *timeDelay);
+
+#endif

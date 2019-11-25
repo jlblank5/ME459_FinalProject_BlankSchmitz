9de2a940c26ae8a03136067770d338705329d901
dgschmitz <dgschmitz@wisc.edu>
2019-11-25 11:02:10 -0600

    working filters, both LP and HP, forward and backward

--- butterHP.c
+++ butterHP.c
@@ -18,7 +18,50 @@
 #define PI 3.1416
 #define complex _Complex
 
-void butterLP(int fc, int fs, double *B, double *A){
+void butterHP(int fc, int fs, double *B, double *A){
+    int N = 2;
+    if(fc >= fs/2){
+        printf("The high-pass cutoff frequency must be less than half the sample rate.");
+        exit(100);
+    }// check that cutoff is below half the sample frequency
+
+    double Fc = fs/PI * tan(PI*fc/fs); // continuous pre-warped frequency
+
+    double theta[2];
+    double complex p_lp[2];
+    double complex pa[2];
+    double complex p[2];
+    double b[3] = {1,-2,1};
+    double K;
+
+    int i, k;
+    for( k = 0 ; k < N ; k++ ){
+        // I. Find the poles of normalized analog lowpass filter
+        theta[k] = (2*(k+1)-1)*PI/(2*N);
+
+        // poles of lp with cutoff = 1 rad/s
+        p_lp[k] = (-sin(theta[k])) + (cos(theta[k]))*I;
+
+        // II. transform the poles for hpf
+        pa[k] = 2*PI*Fc/p_lp[k]; // analog hp poles
+
+        // III. Find coefs of digital filter
+        p[k] = (1+pa[k]/(2*fs))/(1-pa[k]/(2*fs)); // poles by bilinear transform
+
+    }// end for, order of filter
+
+    // convert poles and zeros to polynomial coefficients
+    double complex temp;
+    A[0] = 1;
+    temp =  -(p[0] + p[1]);
+    A[1] = creal(temp);
+    temp =  p[0] * p[1];
+    A[2] = creal(temp);
+
+    K = (A[0] - A[1] + A[2])/(b[0] - b[1] + b[2]);
+    for( i = 0 ; i < 3 ; i++ ){
+        B[i] = b[i]*K;
+    }// end for
 
 }// end butterLP
 
@@ -32,25 +75,37 @@ void butterLP(int fc, int fs, double *B, double *A){
 % b = numerator coefficients of digital filter
 % a = denominator coefficients of digital filter
 %
+
 function [b,a]= hp_synth(N,fc,fs);
+
 if fc>=fs/2;
    error('fc must be less than fs/2')
 end
+
+
 % I.  Find poles of normalized analog lowpass filter
 k= 1:N;
 theta= (2*k -1)*pi/(2*N);
 p_lp= -sin(theta) + j*cos(theta);     % poles of lpf with cutoff = 1 rad/s
+
+
 % II.  transform poles for hpf
 Fc= fs/pi * tan(pi*fc/fs);             % continuous pre-warped frequency
 pa= 2*pi*Fc./p_lp;                     % analog hp poles
+
+
 % III.  Find coeffs of digital filter
 % poles and zeros in the z plane
 p= (1 + pa/(2*fs))./(1 - pa/(2*fs));   % poles by bilinear transform
 q= ones(1,N);                          % zeros at z = 1 (f= 0)
+
+
 % convert poles and zeros to polynomial coeffs
 a= poly(p);                   % convert poles to polynomial coeffs a
 a= real(a);
 b= poly(q);                   % convert zeros to polynomial coeffs b
+
+
 % amplitude scale factor for gain = 1 at f = fs/2  (z = -1)
 m= 0:N;
 K= sum((-1).^m .*a)/sum((-1).^m .*b);        % amplitude scale factor
--- filtfilt.c
+++ filtfilt.c
@@ -7,33 +7,60 @@
 
 double *filtfilt(double *unfiltered, int N, int fs, double low, double high){
     double *filtered = (double *) malloc(sizeof(double) * (N));
-    double *temp = (double *) malloc(sizeof(double) * (N));
+    double *temp1 = (double *) malloc(sizeof(double) * (N));
+    double *temp2 = (double *) malloc(sizeof(double) * (N));
     // Get the filter coefficients
     double B[3];
     double A[3];
-    butterLP(low,fs,B,A);
+    double D[3];
+    double C[3];
+    butterLP(high,fs,B,A); // the low-pass filter gets the "high" cutoff
+    butterHP(low,fs,D,C); // the high-pass filter gets the "low" cutoff
 
-    // pass data through filter forwards
+    // pass data through low-pass filter forwards
     int i, j;
     for( i = 0 ; i < N ; i++ ){
-        temp[i] = B[0] * unfiltered[i];
+        temp1[i] = B[0] * unfiltered[i];
         for( j = 1 ; j < 3 ; j++ ){
-            if(i-j+1 >= 0){
-                temp[i] += B[j]*unfiltered[i-j+1] - A[j]*temp[i-j+1];
+            if(i >= j){
+                temp1[i] += B[j]*unfiltered[i-j] - A[j]*temp1[i-j];
             }//end if
         }// end for j
     }// end for i
 
-    // pass data through filter backwards
-    for( i = 0 ; i > N ; i++ ){
-        filtered[i] = B[0] * temp[N-1-i];
+    // pass data through low-pass filter backwards
+    for( i = 0 ; i < N ; i++ ){
+        temp2[(N-1)-i] = B[0] * temp1[N-1-i];
         for( j = 1 ; j < 3 ; j++ ){
-            if(i-j+1 >= 0){
-                filtered[i] += B[j]*temp[N-1-i+j-1] - A[j]*filtered[i-j+1];
+            if(i >= j){
+                temp2[(N-1)-i] += B[j]*temp1[(N-1)-i+j] - A[j]*temp2[(N-1)-i+j];
             }//end if
         }// end for j
     }// end for i
 
-    return temp;
+    // pass data through high-pass filter forwards
+    for( i = 0 ; i < N ; i++ ){
+        temp1[i] = D[0] * temp2[i];
+        for( j = 1 ; j < 3 ; j++ ){
+            if(i >= j){
+                temp1[i] += D[j]*temp2[i-j] - C[j]*temp1[i-j];
+            }//end if
+        }// end for j
+    }// end for i
+
+    // pass data through high-pass filter backwards
+    for( i = 0 ; i < N ; i++ ){
+        filtered[(N-1)-i] = D[0] * temp1[N-1-i];
+        for( j = 1 ; j < 3 ; j++ ){
+            if(i >= j){
+                filtered[(N-1)-i] += D[j]*temp1[(N-1)-i+j] - C[j]*filtered[(N-1)-i+j];
+            }//end if
+        }// end for j
+    }// end for i
+
+   free(temp1);
+   free(temp2);
+
+    return filtered;
 
 }// end filtfilt

5593996d12fecc1a8c4759036b5b4da54a299b3b
Jon Blank <jlblank@wisc.edu>
2019-12-09 12:49:00 -0500

    beginning of function building to read in options.txt

---
+++ read.c
@@ -0,0 +1,9 @@
+// Author: Jon Blank
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "read.h"
+
+void *read(char *filename,int *numCols,int *numRows){
+
+}
---
+++ read.h
@@ -0,0 +1,8 @@
+// Author: Jon Blank
+
+#ifndef SORT
+#define SORT
+
+void *read(char *filename,int *numCols,int *numRows);
+
+#endif
